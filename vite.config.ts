82979ba8d4c04208550a56821055af9617e7b67a
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-11-30 15:11:06 +0200

    chore: vite init

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Yoga Timer",
+        short_name: "YogaTimer",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
